23987b6cd3b239d08cacfdda610a53fade23c62c
Kevin Carr <cochewen@gmail.com>
2017-04-05 17:57:21 -0400

    small change with button for saving pdf

--- public/script.js
+++ public/script.js
@@ -2,11 +2,8 @@ window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');
-    var COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
-    ];
+
+
     document.getElementById("clickMe").onclick = function () {
       var doc = new jsPDF();
 
@@ -23,6 +20,7 @@ window.onload = function() {
       	'width': 170,
       	'elementHandlers': specialElementHandlers
       });
+      doc.save('markdown.pdf');
     };
 
     // make the tab act like a tab
